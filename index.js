34b0b8682d6680965365de7aa4f56c91ebb04769
John <oleinikov.dev@gmail.com>
2021-07-06 21:43:45 +0300

    changes

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+import express from 'express'
 
-const port = process.env.PORT || 5000
+import cors from 'cors'
+
 const app = express()
-
+app.use(express.json({ extended: true }))
 app.use(cors())
-app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`Server has been started on port: ${port}`)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port: ${PORT}`)
 })
-
-// import express from 'express'
-// import dotenv from 'dotenv'
-// import cors from 'cors'
-
-// import mongoose from 'mongoose'
-// import route from './routes/routes.js'
-// import { redirect } from './controllers/links/redirect.js'
-
-// dotenv.config()
-// const app = express()
-// app.use(express.json({ extended: true }))
-// app.use(cors())
-// app.use('/api', route)
-// app.use('/:code', redirect)
-
-// const PORT = process.env.PORT || 5000
-// const CONNECTION_URL = process.env.DB_URL
-
-// mongoose
-//   .connect(CONNECTION_URL, {
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//   })
-//   .then(() => {
-// app.listen(PORT, () => {
-//   console.log(`Server has been started on port: ${PORT}`)
-// })
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
-// mongoose.set('useFindAndModify', false)
 
 app.get('/', function (req, res) {
   res.send('hello world')
